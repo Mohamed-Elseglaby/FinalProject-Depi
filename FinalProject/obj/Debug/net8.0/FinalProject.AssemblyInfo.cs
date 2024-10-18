38551a48286ed826ae727877516601874d61a3a1
Mohamed Seglaby <mohamedseglaby22@gmail.com>
2024-10-18 04:34:32 +0300

    frontEnd only updates

--- FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e7b8a045f673ff59332f29695035fd96000440e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd6b0c6885cfe99421f740266353a6dac09c02a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
